20c3d9d5c989717d42ddcb86bad90d7a28c64f34
Jake Kara <jake@jakekara.com>
2020-08-26 10:50:24 -0400

    Developer/watch mode working

--- lts-app/scripts/develop.sh
+++ lts-app/scripts/develop.sh
@@ -13,7 +13,14 @@ docker network create lts 2> /dev/null && echo "Created network" || echo "Networ
 echo "Running lts-app container in dev mode"
 docker run --rm -p 7082:7082 \
     --network lts \
-    --name lts-app \
+    --name lts-app-dev-mode \
     -it \
     -v "$(pwd)/let-them-speak":/lts-app \
-    lts-app
+    lts-app /bin/sh -c "gunicorn -b 0.0.0.0:7082 \
+                        --workers 1 \
+                        --threads 8 \
+                        --timeout 30 \
+                        --keep-alive 2 \
+                        --access-logfile - \
+                        --reload \
+                        --log-level=DEBUG server.app:app"
