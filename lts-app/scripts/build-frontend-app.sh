bfb9402c0d5dc6144231fd9194edc606d1acd217
Jake Kara <jake@jakekara.com>
2020-09-14 17:15:52 -0400

    Fixes for GA

--- lts-app/scripts/build-frontend-app.sh
+++ lts-app/scripts/build-frontend-app.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 docker run --rm -it \
-    -v "$(pwd)/let-them-speak":/lts-app \
-    lts-app-frontend-builder \
-    /bin/sh -c "echo Building frontend && \
+-v "$(pwd)/let-them-speak":/lts-app \
+--env LTS_GA_CODE=$LTS_GA_CODE \
+lts-app-frontend-builder \
+/bin/sh -c "echo Building frontend && \
         cd /lts-app && \
         rm -rf node_modules && \
         npm install --no-optional && \
         npm run build && \
-        rm -rf node_modules"
-    
+rm -rf node_modules"
