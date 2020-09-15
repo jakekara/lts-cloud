0fdc362cc3b57906b0bfb2bce099107c5cee32ac
Jake Kara <jake@jakekara.com>
2020-09-15 15:33:54 -0400

    run*.sh scripts - stop containers if running.

--- mongo/scripts/run.sh
+++ mongo/scripts/run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
-set -e 
+set -e
 
 echo Cleaning up Docker stuff
 docker container prune -f
+docker stop lts-mongo | true
 
-echo Creating an lts network 
+echo Creating an lts network
 docker network create lts 2> /dev/null && echo "Created network" || echo "Network already exists"
 
 echo Running lts-mongo container
-docker run -v migrations:/migrations -p 27017:27017 --rm --network lts --name lts-mongo lts-mongo 
+docker run -v migrations:/migrations -p 27017:27017 --rm --network lts --name lts-mongo lts-mongo
--- tomcat/scripts/run-server.sh
+++ tomcat/scripts/run-server.sh
@@ -1,15 +1,16 @@
-set -e 
+set -e
 
 echo Cleaning up Docker stuff
 docker container prune -f
+docker stop lts-blacklab | true
 
-echo Creating an lts network 
+echo Creating an lts network
 docker network create lts 2> /dev/null && echo "Created network" || echo "Network already exists"
 
 
 docker run \
-    --rm \
-    --network lts \
-    --name lts-blacklab \
-    -p 8888:8080 \
-    lts-blacklab
+--rm \
+--network lts \
+--name lts-blacklab \
+-p 8888:8080 \
+lts-blacklab
